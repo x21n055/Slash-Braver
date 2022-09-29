3d8953844c2a8ceb16273c304da608de6e262504
x21n055 <103401671+x21n055@users.noreply.github.com>
2022-09-30 00:13:44 +0900

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int attackDamage = 40;
+
     public float speed;
+    public float jumpForce;
+    public float jumpTimer;
+    public float checkRadius;
+    public float attackRange = 0.5f;
+
+    public Transform feetPos;
+    public Transform attackPoint;
+    public LayerMask whatIsGround;
+    public LayerMask enemyLayers;
 
     private Animator anime = null;
     private Rigidbody2D rb2d = null;
+    private bool isGround;
+    private bool isJumping;
+    private float jumpTimeCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +33,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //çUåÇ
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+        //ÉWÉÉÉìÉv
+        isGround = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTimer;
+            rb2d.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb2d.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
     }
 
     private void FixedUpdate()
@@ -42,5 +87,38 @@ public class Player : MonoBehaviour
         }
 
         rb2d.velocity = new Vector2(horizontalkey * speed, rb2d.velocity.y);
+
+        anime.SetBool("Jump", isJumping);
+        anime.SetBool("Ground", isGround);
     }
+
+    //çUåÇ
+    void Attack()
+    {
+        anime.SetTrigger("Attack");
+        /*Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDmage(attackDamage);
+        }*/
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == enemyTag)
+        {
+            anime.Play("Player_Damaged");
+            isDown = true;
+        }
+    }*/
+
 }
